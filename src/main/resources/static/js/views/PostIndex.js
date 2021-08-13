aaee82ed9771adb6d067275e232af92496663ec0
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-08-13 14:56:33 -0500

    changes

--- src/main/resources/static/js/views/PostIndex.js
+++ src/main/resources/static/js/views/PostIndex.js
@@ -35,7 +35,7 @@ export default function PostIndex(props) {
             console.log(title)
             console.log(content)
 
-            fetch("http://localhost:8080/posts", {
+            fetch("http://localhost:8080/api/posts", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
