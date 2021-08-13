0aae64e5e3d33d029b2ac768df1314c79b4caeb6
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-08-13 12:11:10 -0500

    changes

--- src/main/resources/static/js/views/PostIndex.js
+++ src/main/resources/static/js/views/PostIndex.js
@@ -4,9 +4,56 @@ export default function PostIndex(props) {
             <h1>Posts Page</h1>
         </header>
         <main>
-            <div>
-                ${props.posts.map(post => `<h3>${post.title}</h3>`).join('')}   
-            </div>
+<form>
+    <div>
+        <div>
+            <span>Post Title: </span>
+        </div>
+        <input id="create-post-title" type="text">
+        <div>
+            <span>Post Content: </span>
+        </div>
+        <input id="create-post-content" type="text">
+    </div>
+    <button id="create-btn">Submit</button>
+</form>
+           <div class="post-container">
+                ${props.posts.map(post =>
+        `<h3 class="post-title" data-id="${post.id}">${post.title}</h3>
+            <h2>${post.content}</h2>`).join('')}   
+                
+          </div>
         </main>
     `;
+
 }
+
+    export function PostsEvent() {
+        $('#create-btn').click(function () {
+            let title = $("#create-post-title").val();
+            let content = $("#create-post-content").val();
+            console.log(title)
+            console.log(content)
+
+            fetch("http://localhost:8080/posts", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                redirect: 'follow',
+                body: JSON.stringify([{
+                    "title": this.title,
+                    "content": this.content
+                }])
+            }).then(function(response) {
+                return response.json();
+            }).then(function(data) {
+                console.log(data);
+            });
+
+        });
+    }
+
+
+
+
